689105610cb0b6b6e2052985ae2b85232701af4d
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-23 23:47:42 +0100

    new recursive helper method

--- bowling/bowling_spec.rb
+++ bowling/bowling_spec.rb
@@ -1,4 +1,4 @@
-def bowling_scores(scores)
+def bowling_scores_first_attempt(scores)
   elem_doubles = []
   scores.each_with_index do |elem, index|
     if index % 2 == 0 && elem == 10
@@ -15,6 +15,23 @@ def bowling_scores(scores)
   end
   scores.inject(:+)
 end
+
+def rec_score_helper xs
+  if xs.length <= 3
+    xs
+  elsif xs[0] == 10
+    xs[0,1] + xs[1,2] + rec_score_helper(xs[1..-1])
+  elsif xs[0] + xs[1] == 10
+    xs[0,2] + xs[2,1] + rec_score_helper(xs[2..-1])
+  else
+    xs[0,2] + rec_score_helper(xs[2..-1])
+  end
+end
+
+def bowling_scores lines
+  rec_score_helper(lines).inject(:+)
+end
+
 
 RSpec.describe "Bowling Scores" do
 
@@ -39,7 +56,9 @@ RSpec.describe "Bowling Scores" do
 
     expect(bowling_scores([1,2,3,4,1,9,3,7,1,2,3,4,0,2,3,4,5,5,3,4])).to eq(73)
 
-    expect(bowling_scores([1,2,3,5,5,2,3,4,1,2,3,4,0,2,3,4,1,2,3,7,4])).to eq(65)
+    # this test is wrong
+    # expect(bowling_scores([1,2,3,5,5,2,3,4,1,2,3,4,0,2,3,4,1,2,3,7,4])).to eq(65)
+    expect(bowling_scores([1,2,3,5,5,2,3,4,1,2,3,4,0,2,3,4,1,2,3,7,4])).to eq(61)
   end
 
   it "ends the frame & doubles scores of following two rolls if a strike occurs" do
@@ -52,6 +71,9 @@ RSpec.describe "Bowling Scores" do
     expect(bowling_scores([10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,0])).to eq(20)
   end
 
+  it "scores a mixed game" do
+    rolls = [2,4,5,3,1,6,8,2,0,2,6,4,10,7,3,0,10,6,4,7]
+    expect(bowling_scores rolls).to eq 116
+  end
+
 end
-
-
