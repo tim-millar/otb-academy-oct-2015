977cd73723288ff5d90d4b84343d601fcc83fb3a
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-23 10:57:25 +0100

    triangle 2

--- the-language/lib/triangle.rb
+++ the-language/lib/triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if (a==b && b==c)
+  z,x,y = [a,b,c].sort.reverse
+  if z >= x + y
+    raise TriangleError
+  elsif z <= 0
+    raise TriangleError
+  elsif (a==b && b==c)
     :equilateral
   elsif (a==b || b==c || a==c)
     :isosceles
