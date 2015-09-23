8cacc8469099d204ebae9bb57362af4e9a36e92f
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-23 14:29:26 +0100

    inheritance

--- the-language/spec/inheritance_spec.rb
+++ the-language/spec/inheritance_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "inheritance" do
-  class Dog
+  class AnotherDog
     attr_reader :name
 
     def initialize(name)
@@ -11,7 +11,7 @@ RSpec.describe "inheritance" do
     end
   end
 
-  class Chihuahua < Dog
+  class Chihuahua < AnotherDog
     def wag
       :happy
     end
@@ -22,37 +22,37 @@ RSpec.describe "inheritance" do
   end
 
   it "sets the subclass parent as an ancestor" do
-    expect( Chihuahua.ancestors.include?(Dog) ).to eq( __ )
+    expect( Chihuahua.ancestors.include?(AnotherDog) ).to eq( true )
   end
 
   it "will ultimately inherit from Object" do
-    expect( Chihuahua.ancestors.include?(Object) ).to eq( __ )
+    expect( Chihuahua.ancestors.include?(Object) ).to eq( true )
   end
 
   it "inherits behaviour from a parent class" do
     chico = Chihuahua.new("Chico")
-    expect( chico.name ).to eq( __ )
+    expect( chico.name ).to eq( "Chico" )
   end
 
   it "can add behaviour in subclass, not in a parent class" do
     chico = Chihuahua.new("Chico")
-    expect( chico.wag ).to eq( __ )
+    expect( chico.wag ).to eq( :happy )
 
     expect {
-      fido = Dog.new("Fido")
+      fido = AnotherDog.new("Fido")
       fido.wag
-    }.to raise_error( __ )
+    }.to raise_error( NoMethodError )
   end
 
   it "can modify behaviour in a subclass" do
     chico = Chihuahua.new("Chico")
-    expect( chico.bark ).to eq( __ )
+    expect( chico.bark ).to eq( "yip" )
 
-    fido = Dog.new("Fido")
-    expect( fido.bark ).to eq( __ )
+    fido = AnotherDog.new("Fido")
+    expect( fido.bark ).to eq( "WOOF" )
   end
 
-  class BullDog < Dog
+  class BullDog < AnotherDog
     def bark
       super + ", GROWL"
     end
@@ -60,10 +60,10 @@ RSpec.describe "inheritance" do
 
   it "can invoke the parent behaviour using super" do
     ralph = BullDog.new("Ralph")
-    expect( ralph.bark ).to eq( __ )
+    expect( ralph.bark ).to eq( "WOOF, GROWL" )
   end
 
-  class GreatDane < Dog
+  class GreatDane < AnotherDog
     def growl
       super.bark + ", GROWL"
     end
@@ -73,7 +73,7 @@ RSpec.describe "inheritance" do
     george = GreatDane.new("George")
     expect {
       george.growl
-    }.to raise_error( __ )
+    }.to raise_error( NoMethodError )
   end
 
 end
