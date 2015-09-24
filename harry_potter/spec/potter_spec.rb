6fa4ea1db7fc230361b92f25f69a24639861962b
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-24 11:36:46 +0100

    initial commit - first test suite

---
+++ harry_potter/spec/potter_spec.rb
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+def prices xs
+  return 0    if xs.inject(:+) == 0
+  return 8    if xs.inject(:+) == 1
+  return 15.2 if xs.inject(:+) == 2
+  return 21.6 if xs.inject(:+) == 3
+  return 25.6 if xs.inject(:+) == 4
+  return 30   if xs.inject(:+) == 5
+end
+
+RSpec.describe "Harry Potter prices function" do
+
+  it "costs nothing to buy no books" do
+    expect(prices([0,0,0,0,0])).to eq(0)
+  end
+
+  it "charges £8 for one book" do
+    expect(prices([1,0,0,0,0])).to eq(8)
+    expect(prices([0,0,1,0,0])).to eq(8)
+    expect(prices([0,0,0,0,1])).to eq(8)
+  end
+
+  it "discounts price by 5% if two different books are bought" do
+    expect(prices([1,0,0,1,0])).to eq(15.20)
+    expect(prices([0,0,1,1,0])).to eq(15.20)
+  end
+
+  it "discounts price by 10% if three different books are bought" do
+    expect(prices([1,0,1,1,0])).to eq(21.6)
+    expect(prices([0,0,1,1,1])).to eq(21.6)
+  end
+
+  it "discounts price by 20% if four different books are bought" do
+    expect(prices([1,0,1,1,1])).to eq(25.6)
+    expect(prices([0,1,1,1,1])).to eq(25.6)
+  end
+  
+  it "discounts price by 25% if four different books are bought" do
+    expect(prices([1,1,1,1,1])).to eq(30)
+  end
+  
+end
