eac802fc14b2618c022888b6b407bdbc952e9265
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-25 11:47:05 +0100

    added class in solution file -- all tests pass

--- harry_potter/spec/potter_spec.rb
+++ harry_potter/spec/potter_spec.rb
@@ -95,54 +95,54 @@ end
 RSpec.describe "Harry Potter prices function" do
 
   it "costs nothing to buy no books" do
-    expect(prices([0,0,0,0,0])).to eq(0)
+    expect(HarryPotter.price_order([0,0,0,0,0])).to eq(0)
   end
 
   it "charges £8 for one book" do
-    expect(prices([1,0,0,0,0])).to eq(8)
-    expect(prices([0,0,1,0,0])).to eq(8)
-    expect(prices([0,0,0,0,1])).to eq(8)
+    expect(HarryPotter.price_order([1,0,0,0,0])).to eq(8)
+    expect(HarryPotter.price_order([0,0,1,0,0])).to eq(8)
+    expect(HarryPotter.price_order([0,0,0,0,1])).to eq(8)
   end
 
   it "discounts price by 5% if two different books are bought" do
-    expect(prices([1,0,0,1,0])).to eq(15.20)
-    expect(prices([0,0,1,1,0])).to eq(15.20)
+    expect(HarryPotter.price_order([1,0,0,1,0])).to eq(15.20)
+    expect(HarryPotter.price_order([0,0,1,1,0])).to eq(15.20)
   end
 
   it "discounts price by 10% if three different books are bought" do
-    expect(prices([1,0,1,1,0])).to eq(21.6)
-    expect(prices([0,0,1,1,1])).to eq(21.6)
+    expect(HarryPotter.price_order([1,0,1,1,0])).to eq(21.6)
+    expect(HarryPotter.price_order([0,0,1,1,1])).to eq(21.6)
   end
 
   it "discounts price by 20% if four different books are bought" do
-    expect(prices([1,0,1,1,1])).to eq(25.6)
-    expect(prices([0,1,1,1,1])).to eq(25.6)
+    expect(HarryPotter.price_order([1,0,1,1,1])).to eq(25.6)
+    expect(HarryPotter.price_order([0,1,1,1,1])).to eq(25.6)
   end
   
   it "discounts price by 25% if four different books are bought" do
-    expect(prices([1,1,1,1,1])).to eq(30)
-    expect(prices([3,3,3,3,3])).to eq(90)
+    expect(HarryPotter.price_order([1,1,1,1,1])).to eq(30)
+    expect(HarryPotter.price_order([3,3,3,3,3])).to eq(90)
   end
 
   it "handles combinations of books properly" do
-    expect(prices([1,2,2,0,1])).to eq(40.80)
-    expect(prices([7,2,0,0,3])).to eq(90.40)
-    expect(prices([0,15,0,2,0])).to eq(134.4)
+    expect(HarryPotter.price_order([1,2,2,0,1])).to eq(40.80)
+    expect(HarryPotter.price_order([7,2,0,0,3])).to eq(90.40)
+    expect(HarryPotter.price_order([0,15,0,2,0])).to eq(134.4)
 
-    expect(prices([2,2,2,1,1])).to eq(51.2)
-    expect(prices([5,5,4,2,1])).to eq(113.60)
+    expect(HarryPotter.price_order([2,2,2,1,1])).to eq(51.2)
+    expect(HarryPotter.price_order([5,5,4,2,1])).to eq(113.60)
   end
 
-  it "maintains invariant: prices xs == prices xs.reverse == prices xs.shuffle" do
-    expect(prices([3,3,3,4,3])).to eq(prices([3,3,3,4,3].reverse))
-    expect(prices([2,2,2,1,1])).to eq(prices([2,2,2,1,1].reverse))
-    expect(prices([5,5,4,2,1])).to eq(prices([5,5,4,2,1].reverse))
-    expect(prices([5,4,3,2,1])).to eq(prices([5,4,3,2,1].reverse))
+  it "maintains invariant: HarryPotter.price_order xs == HarryPotter.price_order xs.reverse == HarryPotter.price_order xs.shuffle" do
+    expect(HarryPotter.price_order([3,3,3,4,3])).to eq(HarryPotter.price_order([3,3,3,4,3].reverse))
+    expect(HarryPotter.price_order([2,2,2,1,1])).to eq(HarryPotter.price_order([2,2,2,1,1].reverse))
+    expect(HarryPotter.price_order([5,5,4,2,1])).to eq(HarryPotter.price_order([5,5,4,2,1].reverse))
+    expect(HarryPotter.price_order([5,4,3,2,1])).to eq(HarryPotter.price_order([5,4,3,2,1].reverse))
 
-    expect(prices([3,3,3,4,3])).to eq(prices([3,3,3,4,3].shuffle))
-    expect(prices([2,2,2,1,1])).to eq(prices([2,2,2,1,1].shuffle))
-    expect(prices([5,5,4,2,1])).to eq(prices([5,5,4,2,1].shuffle))
-    expect(prices([5,4,3,2,1])).to eq(prices([5,4,3,2,1].shuffle))
+    expect(HarryPotter.price_order([3,3,3,4,3])).to eq(HarryPotter.price_order([3,3,3,4,3].shuffle))
+    expect(HarryPotter.price_order([2,2,2,1,1])).to eq(HarryPotter.price_order([2,2,2,1,1].shuffle))
+    expect(HarryPotter.price_order([5,5,4,2,1])).to eq(HarryPotter.price_order([5,5,4,2,1].shuffle))
+    expect(HarryPotter.price_order([5,4,3,2,1])).to eq(HarryPotter.price_order([5,4,3,2,1].shuffle))
 
   end
   
