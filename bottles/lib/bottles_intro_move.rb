e81d263f783ca4925a9b05d7b39058db526dea9a
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-28 15:23:15 +0100

    refactored bottles

--- bottles/lib/bottles_intro_move.rb
+++ bottles/lib/bottles_intro_move.rb
@@ -1,15 +1,14 @@
 class Bottles
   def verse(num)
+    nextbnum = num.pred
       "#{no_more(num).capitalize} #{bottles_or_bottle(num)} of beer on the wall, #{no_more(num)} #{bottles_or_bottle(num)} of beer.\n" +
-        "#{action(num)}, #{bottles_remaining(num)} of beer on the wall.\n"
+        "#{action(num)}, #{no_more(nextbnum)} #{bottles_or_bottle(nextbnum)} of beer on the wall.\n"
   end
 
   def verses(num, num2)
-    result = ""
-    num.downto(num2).each do |v|
-      result += verse(v) + "\n"
-    end
-    result
+    num.downto(num2).inject("") { |result, v|
+      result + verse(v) + "\n"
+    }
   end
 
   def sing
@@ -18,64 +17,64 @@ class Bottles
 
   private
   def action(num)
-    BottleNum.new.action(num)
+    BottleNum.new(num).action
   end
 
   def bottles_or_bottle(num)
-    BottleNum.new.bottles_or_bottle(num)
+    BottleNum.new(num).bottles_or_bottle
   end
 
   def it_or_one(num)
-    BottleNum.new.it_or_one(num)
+    BottleNum.new(num).it_or_one
   end
 
   def bottles_remaining(num)
-    BottleNum.new.bottles_remaining(num)
+    BottleNum.new(num).bottles_remaining
   end
 
   def decrement(num)
-    BottleNum.new.decrement(num)
+    BottleNum.new(num).decrement
   end
 
   def no_more(num)
-    BottleNum.new.no_more(num)
+    BottleNum.new(num).no_more
   end
   
 end
 
 class BottleNum
 
-  def action(num)
-    if num == 0
+  def initialize(num)
+    @num = num
+  end
+
+  def one_less_then_num
+    @num - 1
+  end
+
+  def action
+    if @num == 0
       "Go to the store and buy some more"
     else
-      "Take #{it_or_one(num)} down and pass it around"
+      "Take #{it_or_one} down and pass it around"
     end
   end
 
-  def it_or_one(num)
-    num == 1 ? "it" : "one"
+  def it_or_one
+    @num == 1 ? "it" : "one"
   end
   
-  def bottles_or_bottle(num)
-    num == 1 ? "bottle" : "bottles"
+  def bottles_or_bottle
+    @num == 1 ? "bottle" : "bottles"
   end
 
-  def bottles_remaining(num)
-    if num == 1
-      "no more bottles"
-    else
-      "#{decrement(num)} #{bottles_or_bottle(num - 1)}"
-    end
-  end
-  
-  def decrement(num)
-    return 99 if num == 0
-    num - 1
+  def decrement
+    return 99 if @num == -1
+    @num
   end
 
-  def no_more(num)
-    num == 0 ? "no more" : "#{num}"
+  def no_more
+    @num == 0 ? "no more" : "#{decrement}"
   end
   
   
