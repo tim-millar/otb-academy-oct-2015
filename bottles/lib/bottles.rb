a4ac67792ed2baf100626a547622507a7a9f16d2
Tim Millar <heteroskedasticity99@gmail.com>
2015-09-28 11:11:53 +0100

    Wrote the bottles app

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,2 +1,25 @@
 class Bottles
+
+  def initialize; end
+
+  def verse(n)
+    if n == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    elsif n == 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif n == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+    end
+  end
+
+  def verses(n, m)
+    n.downto(m).map { |bottle| verse(bottle) + "\n" }.join('')
+  end
+
+  def sing
+    verses(99,0)
+  end
+  
 end
